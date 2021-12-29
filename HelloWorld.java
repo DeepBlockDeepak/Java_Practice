545b11fa2ecb723b05d7c863703633bff6b7b6cf
DeepBlockDeepak <jordan.medina1729@gmail.com>
2021-12-28 21:58:43 -0700

    updated Java notes

---
+++ HelloWorld.java
@@ -0,0 +1,60 @@
+/* 
+What is this syntax doing?
+
+A class whose name must match the .java filename is created.
+
+Within this class, the driver function is found, I presume.
+
+So how does the public class get called?
+
+*/
+
+
+/*
+.length()
+.concat()
+.equals()
+.equalsIgnoreCase()
+.indexOf()
+.charAt()
+.substring()
+.toUpperCase() / .toLowerCase()
+
+String.valueOf(anIntVar)    String.valueOf(5.99) == "5.99"
+*/
+
+
+public class HelloWorld{
+
+
+
+
+    public static void main(String[] args){
+        
+        String name = args[0];
+
+        printer(name);
+        
+        String bob = "Bob";
+        int i = 0;
+        
+        for(;i < 20; i++){
+            bob = bob.concat("+-+" + i);
+    
+        }
+
+        System.out.println(bob.length());
+
+        
+            
+        //System.out.println(bob);
+        //System.out.println(String.valueOf(i));  // conversion of int to String for printing
+
+    }
+
+    public static void printer(String printedWord){
+        System.out.println("You just printed " + printedWord);
+    }
+
+
+}
