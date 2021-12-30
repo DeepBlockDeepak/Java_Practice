bd2feb5771ed4abf0f350ab8ca7949e3b4a4f304
DeepBlockDeepak <jordan.medina1729@gmail.com>
2021-12-30 15:06:29 -0700

    just farting around, no big changes

--- HelloWorld.java
+++ HelloWorld.java
@@ -20,42 +20,77 @@ So how does the public class get called?
 .substring()
 .toUpperCase() / .toLowerCase()
 
-String.valueOf(anIntVar)    String.valueOf(5.99) == "5.99"
+String.valueOf(anIntVar or int)    String.valueOf(5.99) == "5.99"
 SomeString.compareTo(SomeOtherString)   // obtains alphabetical ordering of the two Strings (greater than, less than, or equal to 0)
 */
 
 
 public class HelloWorld{
 
-
-
-
     public static void main(String[] args){
         
-        String name = args[0];
-
-        printer(name);
+        /*
+        following used for testing out command line input and accepting as a variable 
+        */
+        // String name = args[0];
+        //printer(name);
         
-        String bob = "Bob";
-        int i = 0;
-        
-        for(;i < 20; i++){
-            bob = bob.concat("+-+" + i);
-    
-        }
+        String[] test_array = new String[] {"stuff", "blobs", "third object"};
 
-        System.out.println(bob.length());
+        System.out.println(String.format("The test_array has %d elements.", test_array.length));    //using `String.format()` as an `f` string idea
 
-        
-            
-        //System.out.println(bob);
-        //System.out.println(String.valueOf(i));  // conversion of int to String for printing
+        array_printer(test_array);
 
     }
+
+
 
     public static void printer(String printedWord){
         System.out.println("You just printed " + printedWord);
     }
 
+
+    /**
+     *  This function employs the ternary operation method for array printing 
+     * 
+     * @param arr The String array to be printed
+     */
+    public static void array_printer(String[] arr){
+        
+        System.out.println("{");
+        for(int i = 0; i < arr.length; i++){
+            System.out.println(
+                i == arr.length -1  ? arr[i].toUpperCase() + "\n}"
+                                    : arr[i] + ","
+            );
+        }
+         
+    }
+
+    /* This Method DOESN'T WORK
+            i == arr.length - 1 ? System.out.println(arr[i] + "\n}")
+                                : System.out.println(", " + arr[i]);
+    */
+
+
+
+
+    
+    /**
+     *  
+     *  STUDY HOW THIS ROTATION WORKS!!!!!!!!!!!!
+     * 
+     * @param groceryList
+     */
+    public static void rotateGroceries(String[] groceryList) {
+        if (groceryList.length > 1) {
+          String previous = groceryList[groceryList.length - 1];
+          for (int i = 0; i < groceryList.length; i++) {
+            String temp = groceryList[i];
+            groceryList[i] = previous;
+            previous = temp;
+          }
+        }
+      }
 
 }
